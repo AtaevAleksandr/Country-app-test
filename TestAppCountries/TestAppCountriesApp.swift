da0ed2dafe428c328dd67a31581d7485f3407e74
Aleksandr Ataev <onlyaa94@mail.ru>
2024-12-05 19:35:14 +0300

    Initial Commit

---
+++ TestAppCountries/TestAppCountriesApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestAppCountriesApp.swift
+//  TestAppCountries
+//
+//  Created by Aleksandr Ataev on 05.12.2024.
+//
+
+import SwiftUI
+
+@main
+struct TestAppCountriesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
